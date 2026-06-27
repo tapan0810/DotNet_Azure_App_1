e3097ece35bfe8ed4b3ec6f1bbfe6a9d45aa44e7
Sakshi <sakshi.jun04@gmail.com>
2026-06-27 19:08:18 +0530

    Add project files.

---
+++ DotNet_Azure_App_1/Data/AzureApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNet_Azure_App_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_Azure_App_1.Data
+{
+    public class AzureApplicationDbContext : DbContext
+    {
+        public AzureApplicationDbContext(DbContextOptions<AzureApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AzureUser> AzureUsers =>Set<AzureUser>();
+    }
+}
---
+++ DotNet_Azure_App_1/Models/AzureUser.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet_Azure_App_1.Models
+{
+    public class AzureUser
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserRole { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = false;
+
+    }
+}
