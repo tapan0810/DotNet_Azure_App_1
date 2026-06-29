0c95d05db2780b300f389ddcb2f989f80b07cfef
Tapan Ray <arceus0810@gmail.com>
2026-06-29 21:53:13 +0530

    Update AzureUser.cs

--- DotNet_Azure_App_1/Models/AzureUser.cs
+++ DotNet_Azure_App_1/Models/AzureUser.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string UserName { get; set; } = string.Empty;
+
         public string UserEmail { get; set; } = string.Empty;
         public string UserRole { get; set; } = string.Empty;
         public bool IsActive { get; set; } = false;
