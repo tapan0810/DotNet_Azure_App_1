f33de8640181113b785cb10d90619e111c9c6cf3
Tapan Ray <arceus0810@gmail.com>
2026-09-08 23:07:08 +0530

    Implement missing Azure user deletion and harden CRUD service

--- DotNet_Azure_App_1/Services/AzureUserService.cs
+++ DotNet_Azure_App_1/Services/AzureUserService.cs
@@ -4,67 +4,58 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DotNet_Azure_App_1.Services
 {
-    public class AzureUserService (AzureApplicationDbContext _context): IAzureUserService
+    public class AzureUserService(AzureApplicationDbContext _context) : IAzureUserService
     {
         public async Task<AzureUser> CreateUserAsync(AzureUser user)
         {
-            if(user is null)
-            {
-                return null;
+            ArgumentNullException.ThrowIfNull(user);
 
-            }
             await _context.AzureUsers.AddAsync(user);
             await _context.SaveChangesAsync();
 
             return user;
         }
 
-        public Task<bool> DeleteUserAsync(int id)
+        public async Task<bool> DeleteUserAsync(int id)
         {
-            throw new NotImplementedException();
+            var user = await _context.AzureUsers.FirstOrDefaultAsync(x => x.Id == id);
+            if (user is null)
+            {
+                return false;
+            }
+
+            _context.AzureUsers.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
         }
 
         public async Task<IEnumerable<AzureUser>> GetAllUsersAsync()
         {
-            var users = await _context.AzureUsers.ToListAsync();
-
-            if (users.Count == 0 || users is null)
-            {
-                return null;
-            }
-
-            return users;
+            return await _context.AzureUsers.AsNoTracking().ToListAsync();
         }
 
         public async Task<AzureUser?> GetUserByIdAsync(int id)
         {
-            var user = await _context.AzureUsers.FirstOrDefaultAsync(x => x.Id == id);
-
-            if (user is null)
-            {
-                return null;
-            }
-
-            return user;
+            return await _context.AzureUsers.FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<AzureUser> UpdateUserAsync(AzureUser user, int id)
         {
-            var users = await _context.AzureUsers.FirstOrDefaultAsync(x => x.Id == id);
+            ArgumentNullException.ThrowIfNull(user);
 
-            if (users is null || user is null)
+            var existingUser = await _context.AzureUsers.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingUser is null)
             {
-                return null;
+                return null!;
             }
 
-            users.UserName = user.UserName;
-            users.UserRole = user.UserRole;
-            users.UserEmail = user.UserEmail;
-            users.IsActive = user.IsActive;
+            existingUser.UserName = user.UserName;
+            existingUser.UserRole = user.UserRole;
+            existingUser.UserEmail = user.UserEmail;
+            existingUser.IsActive = user.IsActive;
 
             await _context.SaveChangesAsync();
-
-            return users;
+            return existingUser;
         }
     }
 }
